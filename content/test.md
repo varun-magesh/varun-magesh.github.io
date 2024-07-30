587d43f70807d3b0bf39295aa9ae10f2bb202491
Varun Magesh <vim@law.stanford.edu>
2024-07-29 22:12:46 -0700

    styling and stuff

---
+++ content/test.md
@@ -0,0 +1,5 @@
+Title: Notes from the Kitchen
+Date: 2024-07-28
+Category: Notes
+
+Following is notes
---
+++ pelicanconf.py
@@ -0,0 +1,38 @@
+AUTHOR = 'Varun Magesh'
+SITENAME = 'Varun'
+SITEURL = "http://127.0.0.1:8000"
+STATIC_PATHS = ['./images', './themes/simple/static', './static']
+
+PATH = "content"
+
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    ("You can modify those links in your config file", "#"),
+)
+
+# Social widget
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+
+THEME = "themes/simple"
